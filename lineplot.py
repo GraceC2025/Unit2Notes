1d21bd78f0fb83f8d54777c12ec4b22841618bbc
GraceC2025 <grace.corathers2025@gbwl.org>
2024-10-22 19:10:24 +0000

    added stuff

--- lineplot.py
+++ lineplot.py
@@ -14,11 +14,20 @@ plt.plot(x_vals, np.cos(x_vals))
 
 # save figure as png in dictionary
 plt.savefig('lineplot.png')
+plt.close()
 
 # alternatively set up a figure object for the plot
 fig = plt.figure()
 ax = plt.axes()
 
 # plot a function on the axes object instance
-ax.plot(x_vals, 2*(x_vals), color = "turquoise")
+ax.plot(x_vals, 1.5*(x_vals), ':c')
 plt.savefig('lineplot2.png')
+
+# demo with customization
+plt.plot(x_vals, np.sin(x_vals))
+plt.xlim(-1, 11)
+plt.ylim(-1.5, 1.5)
+plt.axis('tight') # change axis to fit plot better
+
+plt.savefig('lineplot3.png')
